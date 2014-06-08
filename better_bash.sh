8f31a083a735a3bd24a8a8217b9913217577b9a7
jasonrhaas <jasonrhaas@gmail.com>
2014-06-08 16:01:21 -0400

    added better_bash.sh

---
+++ better_bash.sh
@@ -0,0 +1,18 @@
+# This script adds aliases and other customizations to make bash faster 
+
+# In order to add this to your environment you will need to source this file.
+# Just add this file anywhere you like such as your home directory or
+# /etc/profile.d if you wish to add it for all users on the system.
+
+# Then add this block of code to your ~/.bashrc file.  The located of the git.sh
+# file is dependent upon when you put it.
+
+#    if [ -f ~/better_bash.sh ]; then
+#        . ~/better_bash.sh
+#    fi
+
+alias p='pushd'
+alias d='dirs -v'
+alias ll='ls -laF'
+dc() { builtin cd "$@" && ls; } # 'dc' will change directory and then do an 'ls'
+
