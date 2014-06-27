649f4cf3fc0377a8ba3a8607f238b68cf7c22923
jasonrhaas <jasonrhaas@gmail.com>
2014-06-27 15:00:28 -0400

    adding create users script

---
+++ build/create_users.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+useradd aholling -c "Andrew Hollingsworth"
+useradd mchaney -c "Mark Chaney"
+useradd lkannard -c "Lance Kannard"
+useradd rthompso -c "Roy Thompson"
+useradd lsmith -c "Lucas Smith"
+useradd jcartmil -c "Jared Cartmill"
+useradd mwenske -c "Michael Wenske"
+useradd gelee -c "Geun Lee"
+
+
