4f45ea0377203563472406e4f10eb6bb5ee4f82f
Jason Haas <jasonrhaas@gmail.com>
2015-02-10 11:16:51 -0500

    git aware improvements

--- build/git-aware-prompt.sh
+++ build/git-aware-prompt.sh
@@ -1,3 +1,3 @@
-export GITAWAREPROMPT=~/.bash/git-aware-prompt
+export GITAWAREPROMPT=~/repos/bash-perl-scripts/build
 source $GITAWAREPROMPT/main.sh
 export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
